13fbb2912cc12ffcd6a8a66914c68cfa87bada32
Tyler Haven <ty@slh1.com>
2022-05-23 20:02:11 -0600

    Display numbers when clicked

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let buttons = document.querySelectorAll('.numbers')
+let display = document.querySelector('.display')
+let runningDisplay = []
+
 function add (num1, num2) {
     return num1 + num2
-
 }
 
 function subtract (num1, num2) {
@@ -14,3 +17,12 @@ function multiply (num1, num2) {
 function divide (num1, num2) {
     return num1 / num2
 }
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        runningDisplay.push(button.textContent)
+        console.log(runningDisplay)
+        display.textContent = runningDisplay.toString()
+                                            .replace(/\D/g, '')
+    })
+})
